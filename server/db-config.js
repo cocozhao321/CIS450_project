9474417e1596b566018fdb5020ab2d6a943fde3e
Brandy Yuan <yuanb@seas.upenn.edu>
2021-04-22 10:34:27 -0400

    added DB credentials

---
+++ server/db-config.js
@@ -0,0 +1,8 @@
+// Private. Will not be included in submission
+// Replace <Your Pennkey> and <Your Password>
+module.exports = {
+  host: "cis450-project.cuhwydxfgckt.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "CIS450project!",
+  database: "project_db"
+};
